7ff52596c70d41a0d43dc2bbbf7b31af75f0c529
VickyDuarte1 <victoria.durte@gmail.com>
2022-12-22 22:38:57 -0300

    -commit1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import logocontador from './imagenes/DALL·E 2022-12-22 20.34.17 - Logo fondo oscuro cartel neon Click.png';
+import {useState} from 'react';
 
 function App() {
+
+const [numClicks, setNumClicks ] = useState(0); 
+
+const manejarClick = () => {
+  setNumClicks(numClicks +1);
+}
+
+const reiniciarContador = () => {
+  setNumClicks(0);
+}
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     <div className='Logo'>
+      <img className='logoimg'
+      src={logocontador}
+      alt='Logodelcontador'/>
+     </div>
+
+     <div className='contenedor-de-botones'>
+     <Contador numClicks={numClicks}/>
+     <Boton 
+     texto= 'Click'
+     esBotonDeClick={true}
+     manejarClick={manejarClick}/>
+     <Boton 
+     texto='Reiniciar'
+     esBotonDeClick={false}
+     manejarClick={reiniciarContador}/>
+     </div>
+
     </div>
   );
 }
